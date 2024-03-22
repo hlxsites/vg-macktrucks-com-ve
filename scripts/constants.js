c31dfc5d954a017fa075d13148f37b8f1d349a77
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-03-22 15:52:00 +0100

    Null at footer fix & constants.js added

---
+++ scripts/constants.js
@@ -0,0 +1,15 @@
+// check if an active campaign is running or OneTrust needs scan the active scripts
+export const COOKIE_CHECK = false;
+
+// ONE TRUST COOKIE CONSENT
+export const DATA_DOMAIN_SCRIPT = '447123da-e24f-4ee7-833a-52523c4c8640';
+
+// GOOGLE TAG MANAGER ID
+export const GTM_ID = 'GTM-5DKKVHFL';
+
+// COOKIE Constants
+export const COOKIE_VALUES = {
+  performance: 'C0002',
+  targeting: 'C0004',
+  social: 'C0005',
+};
